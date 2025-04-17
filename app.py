bdcc32aec5a1a13fb0747429138e7cb5ca766273
Yuliia921 <yuliya-durnina@yandex.ru>
2025-04-17 21:01:04 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
 
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Обслуживаем все файлы из текущей директории (где index.html, js, css и т.д.)
-app.mount("/", StaticFiles(directory=".", html=True), name="static")
+# Обслуживаем статические файлы (если понадобится)
+app.mount("/static", StaticFiles(directory="."), name="static")
+
+@app.get("/")
+async def get_index():
+    return FileResponse("index.html")
+
+@app.get("/pregnancy.html")
+async def get_pregnancy():
+    return FileResponse("pregnancy.html")
+
+@app.get("/consultation.html")
+async def get_consultation():
+    return FileResponse("consultation.html")
+
+@app.get("/main.js")
+async def get_main_js():
+    return FileResponse("main.js")
+
+@app.get("/consultation.js")
+async def get_consultation_js():
+    return FileResponse("consultation.js")
+
+@app.get("/styles.css")
+async def get_styles():
+    return FileResponse("styles.css")
